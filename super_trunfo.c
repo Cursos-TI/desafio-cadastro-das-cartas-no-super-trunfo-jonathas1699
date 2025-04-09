c54d7d5e02c8c0a3a21e21f5d89c75b74a447ad7
jonathas1699 <148777722+jonathas1699@users.noreply.github.com>
2025-04-09 20:35:16 -0300

    Update super_trunfo.c
    Fui atrás de conseguir nomes de cidade com espaço, e é um pouco complicado, mas deu certo, mas pra frente vou tentar colocar com acentos

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,97 +1,121 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
 
+void limpar_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
+
+void ler_string(char *variavel, int tamanho) {
+    fgets(variavel, tamanho, stdin);
+    variavel[strcspn(variavel, "\n")] = '\0';
+}
 
 int main() {
-
+    // Configura locale para aceitar acentos
+    setlocale(LC_ALL, "Portuguese");
     
-    //variáveis da carta 1//
+    // definindo Variáveis da carta 1
     char estado1;
     char codigo1[4];
-    char NomeCidade1[50];
+    char NomeCidade1[100];
     int populacao1;
     float area1;
     float PIB1;
     int pontotur1;
 
-    //Variáveis da carta 2//
+    // Variáveis da carta 2
     char estado2;
     char codigo2[4];
-    char NomeCidade2[50];
+    char NomeCidade2[100];
     int populacao2;
     float area2;
     float PIB2;
     int pontotur2;
 
-
     // Dados da carta 1
-printf("//INSIRA OS DADOS DA PRIMEIRA CARTA//\n");
-printf("Digite a letra do estado (De A a H): ");
-scanf(" %c", &estado1);  // Espaço antes do %c para consumir \n
-printf("Digite a letra do estado e o código (de 01 a 04, EX: A01; B02): ");
-scanf("%3s", codigo1);  // Limita a 3 caracteres para evitar overflow
-printf("Digite o nome da cidade: ");
-scanf(" %s", NomeCidade1);  // Lê até encontrar quebra de linha
-printf("Digite o número da população: ");
-scanf("%d", &populacao1);
-printf("Digite a área (em km²): ");
-scanf("%f", &area1);
-printf("Digite o PIB (em bilhões de reais): ");
-scanf("%f", &PIB1);
-printf("Digite a quantidade de pontos turísticos: ");
-scanf("%d", &pontotur1);
-
-// Limpar o buffer de entrada
-while(getchar() != '\n');
-
-// Dados da carta 2
-printf("\n//INSIRA OS DADOS DA SEGUNDA CARTA//\n");
-printf("Digite a letra do estado (De A a H): ");
-scanf(" %c", &estado2);
-printf("Digite a letra do estado e o código (de 01 a 04, EX: A01; B02): ");
-scanf("%3s", codigo2);
-printf("Digite o nome da cidade: ");
-scanf(" %s", NomeCidade2);
-printf("Digite o número da população: ");
-scanf("%d", &populacao2);
-printf("Digite a área (em km²): ");
-scanf("%f", &area2);
-printf("Digite o PIB (em bilhões de reais): ");
-scanf("%f", &PIB2);
-printf("Digite a quantidade de pontos turísticos: ");
-scanf("%d", &pontotur2);
-
-
-
-    //PARTE DE IMPRIMIR OS DADOS//
-
-    printf("DADOS DA CARTA 1: \n");
-    printf("A letra do estado é: %c \n", estado1);
-    printf("O código é: %s \n", codigo1);
-    printf("O nome da cidade é: %s \n", NomeCidade1);
-    printf("A população da cidade é de: %d \n",populacao1);
-    printf("A área da cidade é de: %f \n", area1);
-    printf("O PIB é de: %f \n", PIB1);
-    printf("A quantidade de pontos turísticos é: %d \n", pontotur1);
-
-
-    printf("DADOS DA CARTA 2: \n");
-    printf("A letra do estado é: %c \n", estado2);
-    printf("O código é: %s \n", codigo2);
-    printf("O nome da cidade é: %s \n", NomeCidade2);
-    printf("A população da cidade é de: %d \n",populacao2);
-    printf("A área da cidade é de: %f \n", area2);
-    printf("O PIB é de: %f \n", PIB2);
-    printf("A quantidade de pontos turísticos é: %d \n", pontotur2);
+    printf("// INSIRA OS DADOS DA PRIMEIRA CARTA //\n");
     
-
- 
-
-
+    printf("Digite a letra do estado (De A a H): ");
+    scanf(" %c", &estado1);
+    limpar_buffer();
     
+    printf("Digite o código (formato A01): ");
+    scanf("%3s", codigo1);
+    limpar_buffer();
+    
+    printf("Digite o nome da cidade: ");
+    ler_string(NomeCidade1, sizeof(NomeCidade1));
+    
+    printf("Digite a população: ");
+    scanf("%d", &populacao1);
+    limpar_buffer();
+    
+    printf("Digite a área (km²): ");
+    scanf("%f", &area1);
+    limpar_buffer();
+    
+    printf("Digite o PIB (bilhões): ");
+    scanf("%f", &PIB1);
+    limpar_buffer();
+    
+    printf("Digite os pontos turísticos: ");
+    scanf("%d", &pontotur1);
+    limpar_buffer();
 
+    // Dados da carta 2
+    printf("\n// INSIRA OS DADOS DA SEGUNDA CARTA //\n");
+    
+    printf("Digite a letra do estado (De A a H): ");
+    scanf(" %c", &estado2);
+    limpar_buffer();
+    
+    printf("Digite o código (formato A01): ");
+    scanf("%3s", codigo2);
+    limpar_buffer();
+    
+    printf("Digite o nome da cidade: ");
+    ler_string(NomeCidade2, sizeof(NomeCidade2));
+    
+    printf("Digite a população: ");
+    scanf("%d", &populacao2);
+    limpar_buffer();
+    
+    printf("Digite a área (km²): ");
+    scanf("%f", &area2);
+    limpar_buffer();
+    
+    printf("Digite o PIB (bilhões): ");
+    scanf("%f", &PIB2);
+    limpar_buffer();
+    
+    printf("Digite os pontos turísticos: ");
+    scanf("%d", &pontotur2);
+    limpar_buffer();
 
+    // PARTE DE IMPRIMIR OS DADOS
+    printf("\n=== DADOS DAS CARTAS ===\n");
+    
+    // Carta 1
+    printf("\nDADOS DA CARTA 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Cidade: %s\n", NomeCidade1);
+    printf("População: %d habitantes\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: R$ %.2f bilhões\n", PIB1);
+    printf("Pontos turísticos: %d\n", pontotur1);
 
+    // Carta 2
+    printf("\nDADOS DA CARTA 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", NomeCidade2);
+    printf("População: %d habitantes\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: R$ %.2f bilhões\n", PIB2);
+    printf("Pontos turísticos: %d\n", pontotur2);
+
+    return 0;
 }
-
-
-
